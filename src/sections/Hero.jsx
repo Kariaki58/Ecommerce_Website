189368decda83e87406c0b5be1b6a9c525e15893
kariaki58 <programmerjavascript901@gmail.com>
2024-04-02 02:53:58 +0100

    updated code

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center p-2 max-container gap-10 min-h-screen">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike Shoes</span>
+        </h1>
+      </div>
+    </section>
   )
 }
 
