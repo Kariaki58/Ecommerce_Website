de1fbef192729db8888d10cb028e18b03aaca910
kariaki58 <programmerjavascript901@gmail.com>
2024-04-05 08:11:07 +0100

    finished nike website landing page

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,31 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
+
 const SpecialOffer = () => {
   return (
-    <div>
-      SpecialOffer
-    </div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="offer" width={773} height={687} className="object-contain"/>
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin capitalize text-4xl font-bold lg:max-w-lg">
+          <span className="text-coral-red inline-block mt-3">Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Inventore libero asperiores optio illo vel eum dolorem unde amet id quidem.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, corrupti exercitationem!
+          Dolorum quidem molestiae deserunt autem est consequuntur,
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="shop now" iconURL={arrowRight} />
+          <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray"/>
+        </div>
+      </div>
+    </section>
   )
 }
 
